7ba744cdd924f5b3539960355998aa0fcf85aca7
norlin <alexey@norlin.guru>
2016-08-22 15:11:44 +0300

    add release command

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function(grunt) {
+  let pkg = grunt.file.readJSON('package.json');
+  let version = pkg.version;
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: {
@@ -23,8 +26,24 @@ module.exports = function(grunt) {
         dest: 'dist/mux.mp4.js'
       },
     },
+    shell: {
+      github: {
+        command: [
+          'git checkout -b tag-v'+version,
+          //'grunt',
+          //'git add dist',
+          //'git commit -m "add dist v'+version+'"',
+          'git tag -a v'+version+' -m "v'+version+'"',
+          'git checkout hola_5.0.2-1',
+          'git branch -D tag-v'+version,
+          'git push --tags origin'
+        ].join('&&')
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-shell');
   grunt.registerTask('default', ['clean', 'concat:mp4']);
+  grunt.registerTask('release', ['default', 'shell:github']);
 };
